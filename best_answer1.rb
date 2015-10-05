0405ea179c800179075f9e1577e9267fa2b1ba24
Leif Steffensen <MoonMediaMiami@Moon-Medias-MacBook.local>
2015-10-05 18:28:31 -0400

    Both homework files

---
+++ best_answer1.rb
@@ -0,0 +1,15 @@
+class Square
+    attr_accessor :side_length
+
+  def initialize(side_length)
+    @side_length = side_length
+
+    def area
+        side_length * side_length
+    end
+  end
+end
+
+s = Square.new(5)
+
+p s.area# git
---
+++ best_answer2.rb
@@ -0,0 +1,24 @@
+CHEETAH_GIRLS = [
+  "Raven-Symone",
+  "Adrienne Bailon",
+  "Sabrina Bryan",
+  "Kiely Williams"
+]
+
+auditions = [
+  "Raven-Symone",
+  "Usher",
+  "Wiz Khalifa",
+  "Adrienne Bailon",
+  "Hulk Hogan",
+  "Sabrina Bryan",
+  "Diego Lugo",
+  "Kiely Williams",
+  "Justin Timberlake"
+]
+
+panel = []
+
+#YOUR CODE HERE
+
+puts panel
